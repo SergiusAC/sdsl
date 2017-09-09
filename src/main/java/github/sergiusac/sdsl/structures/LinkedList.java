836d89981cebae72f84a4091027f3809a8cbeafe
Sergey Cheen <sergeycheen@gmail.com>
2017-09-09 17:48:04 +0600

    Создана первоначальная версия класса LinkedList

---
+++ src/main/java/github/sergiusac/sdsl/structures/LinkedList.java
@@ -0,0 +1,74 @@
+package github.sergiusac.sdsl.structures;
+
+/**
+ * Created by Sergey Cheen on 9/9/17.
+ */
+public class LinkedList<T> {
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void addFront(T item) {
+        Node<T> node = new Node<>(item);
+        if (isEmpty()) {
+            head = node;
+            tail = head;
+        } else {
+            Node<T> oldHead = head;
+            head = node;
+            head.setNext(oldHead);
+            head.setPrev(null);
+            oldHead.setPrev(head);
+        }
+        size++;
+    }
+
+    public void addBack(T item) {
+        Node<T> node = new Node<>(item);
+        if (isEmpty()) {
+            head = node;
+            tail = head;
+        } else {
+            Node<T> oldTail = tail;
+            tail = node;
+            tail.setNext(null);
+            tail.setPrev(oldTail);
+            oldTail.setNext(tail);
+        }
+        size++;
+    }
+
+    public void removeFront() {
+        if (!isEmpty()) {
+            head = head.getNext();
+            head.setPrev(null);
+            size--;
+        }
+    }
+
+    public void removeBack() {
+        if (!isEmpty()) {
+            tail = tail.getPrev();
+            tail.setNext(null);
+            size--;
+        }
+    }
+
+}
