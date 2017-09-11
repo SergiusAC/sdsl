465db0f842b98abd9af1629c56d94f68c694ec81
Sergey Cheen <sergeycheen@gmail.com>
2017-09-11 20:22:37 +0600

    Добавлен класс с различными вспомогательными утилитами.

---
+++ src/main/java/github/sergiusac/sdsl/algorithms/Utilities.java
@@ -0,0 +1,24 @@
+package github.sergiusac.sdsl.algorithms;
+
+/**
+ * Created by Sergey Cheen on 9/11/17.
+ */
+public class Utilities {
+
+    public static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    public static void exchange(Comparable[] arr, int i, int j) {
+        Comparable tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static boolean isSorted(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++)
+            if (less(arr[i], arr[i - 1]))
+                return false;
+        return true;
+    }
+}
