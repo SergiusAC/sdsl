e90a2b71420002d2269283281c79a0e15f420da7
Sergey Cheen <sergeycheen@gmail.com>
2017-09-16 21:35:46 +0600

    Создан интерфейс стандартной приоритетной очереди

---
+++ src/main/java/github/sergiusac/sdsl/interfaces/IPriorityQueue.java
@@ -0,0 +1,18 @@
+package github.sergiusac.sdsl.interfaces;
+
+/**
+ * Created by Sergey Cheen on 9/16/17.
+ */
+public interface IPriorityQueue<K extends Comparable<K>>
+        extends Iterable<K> {
+
+    void insert(K key);
+
+    K pop();
+
+    K top();
+
+    boolean isEmpty();
+
+    int getSize();
+}
