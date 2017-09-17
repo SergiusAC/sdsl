7d9c2574ab9253198f8d5014c0ba037ebefb772b
Sergey Cheen <sergeycheen@gmail.com>
2017-09-17 20:23:06 +0600

    Добавлена новая функция sort, в которую можно передать функциональный интерфейс Order, задающий способ сортировки (по убыванию или по возрастанию)

--- src/main/java/github/sergiusac/sdsl/algorithms/MergeSort.java
+++ src/main/java/github/sergiusac/sdsl/algorithms/MergeSort.java
@@ -1,5 +1,7 @@
 package github.sergiusac.sdsl.algorithms;
 
+import github.sergiusac.sdsl.interfaces.Order;
+
 /**
  * Created by Sergey Cheen on 9/11/17.
  */
@@ -9,19 +11,26 @@ public class MergeSort {
 
     public static void sort(Comparable[] arr) {
         aux = new Comparable[arr.length];
-        sort(arr, 0, arr.length - 1);
+        sort(arr, 0, arr.length - 1, null);
     }
 
-    private static void sort(Comparable[] arr, int lo, int hi) {
+    public static void sort(Comparable[] arr, Order<?> order) {
+        aux = new Comparable[arr.length];
+        sort(arr, 0, arr.length - 1, order);
+    }
+
+    private static void sort(Comparable[] arr, int lo, int hi,
+                             Order order) {
         if (hi > lo) {
             int mid = lo + (hi - lo) / 2;
-            sort(arr, lo, mid);
-            sort(arr, mid + 1, hi);
-            merge(arr, lo, mid, hi);
+            sort(arr, lo, mid, order);
+            sort(arr, mid + 1, hi, order);
+            merge(arr, lo, mid, hi, order);
         }
     }
 
-    private static void merge(Comparable[] arr, int lo, int mid, int hi) {
+    private static void merge(Comparable[] arr, int lo, int mid, int hi,
+                              Order order) {
         int i = lo;
         int j = mid + 1;
         for (int k = lo; k <= hi; k++)
@@ -31,7 +40,9 @@ public class MergeSort {
                 arr[k] = aux[j++];
             else if (j > hi)
                 arr[k] = aux[i++];
-            else if (Utilities.less(arr[j], arr[i]))
+            else if (order == null ?
+                    Utilities.less(aux[j], aux[i]) :
+                    Utilities.less(aux[j], aux[i], order))
                 arr[k] = aux[j++];
             else
                 arr[k] = aux[i++];
